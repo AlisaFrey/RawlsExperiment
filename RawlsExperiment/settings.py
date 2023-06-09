ea8118a1c82a48004d3f6744a98f13b5cfddab4c
Alisa <79513418+AlisaFrey@users.noreply.github.com>
2023-06-09 16:03:28 +0200

    adding ROOMs in settings.py

--- RawlsExperiment/settings.py
+++ RawlsExperiment/settings.py
@@ -31,6 +31,14 @@ SESSION_CONFIG_DEFAULTS = dict(
 PARTICIPANT_FIELDS = ['decision','is_dropout','num_correct', 'fake_num_correct','income']
 SESSION_FIELDS = ['params','players_per_group']
 
+ROOMS = [
+    dict(
+        name='dice_lab',
+        display_name ='DICE Lab',
+        participant_label_file='C:/otree/dicelab_otree_labels.txt'
+    )
+]
+
 # ISO-639 code
 # for example: de, fr, ja, ko, zh-hans
 LANGUAGE_CODE = 'de'
